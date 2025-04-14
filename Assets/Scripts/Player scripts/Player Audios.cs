275ee14f84f88f681fc679932c78db0846cf94ff
fwjchim1 <stomperr7683@gmail.com>
2025-04-13 19:11:19 -0500

    Learned about coroutines and when to use them (used it to make a cooldown on the bullet shooter)
    Also made it so that the shooting sound effects only trigger when you're off cooldown and shoot

--- Assets/Scripts/Player scripts/Player Audios.cs
+++ Assets/Scripts/Player scripts/Player Audios.cs
@@ -8,9 +8,13 @@ public class PlayerAudios : MonoBehaviour
     public AudioSource source; //where the audo is going to be played from
     public List<AudioClip> clip; //the audioclip itself
     public GameObject p;
+    public GameObject shooterScript;
+    public Shooter shooter;
+
+
     void Start()
     {
-        PlayerMovement player = p.GetComponent<PlayerMovement>();
+        shooter = shooterScript.GetComponent<Shooter>();   
     }
 
     // Update is called once per frame
@@ -21,8 +25,19 @@ public class PlayerAudios : MonoBehaviour
             source.PlayOneShot(clip[0]);
         }
 
-        if(Input.GetKeyDown(KeyCode.Mouse0)){
-            source.PlayOneShot(clip[1]);
-        }    
+
+        //this is not working
+        //it's supposed to play the bullet shooting sound effect whenever you left click and not on cooldown
+        //but it never plays no matter what
+        //4/13/2025
+        //if(Input.GetKeyDown(KeyCode.Mouse0) && shooter.getOnCooldown()){
+        //    Debug.Log("Playing shooting sound");
+        //    source.PlayOneShot(clip[1]);
+        //}    
     }
+
+
+
+
+
 }
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,25 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Profiling;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
 
+    [Header("Sound stuff")]
+    public AudioSource shootSFXSource;
+    public List<AudioClip> shootSFXClip;
+
+    [Header("Bullet variables")]
     public float bulletSpeed;
     public GameObject bulletPrefab;
     public Transform shootingPoint;
     public int bulletDamage = 5;
+    public bool onCooldown = false;
+    public float cooldownDuration;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    public bool getOnCooldown(){
+        return onCooldown;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0)){
-            Instantiate(bulletPrefab, shootingPoint);
-            
+        
+        if(Input.GetKeyDown(KeyCode.Mouse0) && !onCooldown){ //if left click and not on cooldown
+            shootSFXSource.PlayOneShot(shootSFXClip[0]);
+            StartCoroutine(Shoot());                         //run the shoot coroutine
         }
+        
+
     }
+
+    IEnumerator Shoot(){
+        Instantiate(bulletPrefab, shootingPoint); //shoot the bullet
+        onCooldown = true;                        //put it on cooldown
+        yield return new WaitForSeconds(cooldownDuration);   //now wait 0.15 seconds
+        onCooldown = false;                       //take it off cooldown (can shoot again now)
+    }
+
+
+
 }
